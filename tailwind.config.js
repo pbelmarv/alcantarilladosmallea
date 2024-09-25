03576dcba29b4b51d3eb21b90292dd31a1e0f2d2
pbelmarv <pbelmarv@hotmail.com>
2024-09-25 18:34:17 -0300

    Commit inicial

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js, jpg, avif}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins']
+      }
+    },
+  },
+  plugins: [],
+}
+
