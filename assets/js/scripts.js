dae053a45bb68b11dba294d7c46287cce0c9a3fb
pbelmarv <pbelmarv@hotmail.com>
2024-09-26 00:16:23 -0300

    Updates

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -25,3 +25,26 @@ const swiper = new Swiper(".dAlcantarillados", {
         clickable: true,
     },
 });
+
+const swiperFosas = new Swiper(".lFosas", {
+    slidesPerView: 1, // Por defecto, en pantallas pequeñas muestra 1 tarjeta
+    spaceBetween: 20, // Espacio entre cada tarjeta
+    breakpoints: {
+        640: {
+            slidesPerView: 1, // En móviles pequeños muestra 1 tarjeta
+            spaceBetween: 10,
+        },
+        768: {
+            slidesPerView: 2, // En pantallas medianas muestra 2 tarjetas
+            spaceBetween: 20,
+        },
+        1280: {
+            slidesPerView: 3, // En pantallas grandes muestra 3 tarjetas
+            spaceBetween: 30,
+        },
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
