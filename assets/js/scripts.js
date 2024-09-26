5803f7128378f1009ec2dc7c6e966679f49a09d1
pbelmarv <pbelmarv@hotmail.com>
2024-09-26 00:20:09 -0300

    Updates

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -48,3 +48,26 @@ const swiperFosas = new Swiper(".lFosas", {
         clickable: true,
     },
 });
+
+const swiperBombas = new Swiper(".bSumergibles", {
+    slidesPerView: 1, // Por defecto, en pantallas pequeñas muestra 1 tarjeta
+    spaceBetween: 20, // Espacio entre cada tarjeta
+    breakpoints: {
+        640: {
+            slidesPerView: 1, // En móviles pequeños muestra 1 tarjeta
+            spaceBetween: 10,
+        },
+        768: {
+            slidesPerView: 2, // En pantallas medianas muestra 2 tarjetas
+            spaceBetween: 20,
+        },
+        1280: {
+            slidesPerView: 3, // En pantallas grandes muestra 3 tarjetas
+            spaceBetween: 30,
+        },
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
